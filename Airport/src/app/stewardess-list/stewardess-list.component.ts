b4092fb79222edad9c4ea7738260065aaf8c8e44
Dmytro <dima.smolts@gmail.com>
2018-07-27 17:02:48 +0300

    stew service

--- Airport/src/app/stewardess-list/stewardess-list.component.ts
+++ Airport/src/app/stewardess-list/stewardess-list.component.ts
@@ -3,6 +3,7 @@ import {Http, Response} from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Stewardess } from '../Stewardess';
+import {StewService} from '../stew.service';
 
 @Component({
   selector: 'app-stewardess-list',
@@ -13,7 +14,8 @@ export class StewardessListComponent implements OnInit {
 
   stewardesses: Stewardess[] = [];
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private service: StewService) {
+    /*
     const url = 'http://localhost:3111/api/stewardess';
     this.stewardesses = [];
     this.http.get(url)
@@ -23,13 +25,15 @@ export class StewardessListComponent implements OnInit {
       console.log(this.stewardesses);
     }, (response) => {
       console.log('failed');
-    });
+    });*/
     }
 
   ngOnInit() {
+    this.service.getStews().subscribe(data => this.stewardesses = data);
   }
 
   DeleteStewardess(id: number) {
+    /*
     console.log(id);
     const url = 'http://localhost:3111/api/stewardess/' + id;
     console.log(url);
@@ -38,7 +42,8 @@ export class StewardessListComponent implements OnInit {
       console.log('in');
     }, (response) => {
       console.log('out');
-    } );
+    } );*/
+    this.service.DeleteStew(id);
     }
 
 }
